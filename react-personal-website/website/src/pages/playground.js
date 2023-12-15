c91bfb7ec30cf26a490cccf7640a57951c100590
Vindorable <j.raj93.designer@gmail.com>
2023-12-15 15:18:39 +0800

    Playground projects tabs improvement.

--- react-personal-website/website/src/pages/playground.js
+++ react-personal-website/website/src/pages/playground.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { NumberCircleFour, NumberCircleOne, NumberCircleThree, NumberCircleTwo } from "phosphor-react";
+import { NumberCircleFive, NumberCircleFour, NumberCircleOne, NumberCircleThree, NumberCircleTwo } from "phosphor-react";
 
 import { NavTabV, NavTabsV } from "../components/nav-tab";
 
@@ -23,6 +23,39 @@ const Playground = () => {
   const [isMouseOver, setIsMouseOver] = React.useState(false);
   const [labelVisibility, setLabelVisibility] = React.useState(false);
 
+  const items = [
+    {
+      pathName: "/test",
+      openInNewTab: false,
+      label: "Pokemon (Fan-Made)",
+      icon: <NumberCircleOne size={26} />,
+    },
+    {
+      pathName: "/test",
+      openInNewTab: false,
+      label: "Mobile Games",
+      icon: <NumberCircleTwo size={26} />,
+    },
+    {
+      pathName: "/test",
+      openInNewTab: false,
+      label: "7 Second Vines",
+      icon: <NumberCircleThree size={26} />,
+    },
+    {
+      pathName: "http://www.google.com/",
+      openInNewTab: true,
+      label: "Clothing (Unruly Gent)",
+      icon: <NumberCircleFour size={26} />,
+    },
+    {
+      pathName: "/test",
+      openInNewTab: false,
+      label: "Don't Stave Together Mods",
+      icon: <NumberCircleFive size={26} />,
+    },
+  ];
+
   return (
     <>
       <Box height={"calc(100vh - 60px)"}>
@@ -35,7 +68,7 @@ const Playground = () => {
               backgroundColor: "#F8FAFF",
               boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
               //height: 100,
-              width: isMouseOver ? "200px" : "72px",
+              width: isMouseOver ? "300px" : "72px",
               transition: theme.transitions.create("all", {
                 easing: theme.transitions.easing.sharp,
                 duration: theme.transitions.duration.leavingScreen
@@ -53,28 +86,16 @@ const Playground = () => {
                 onChange={handleChange}
                 orientation="vertical"
               >
-                <NavTabV icon={<NumberCircleOne size={26} />} iconPosition="start" label={labelVisibility && "Experience"} />
-                <NavTabV icon={<NumberCircleTwo size={26} />} iconPosition="start" label={labelVisibility && "Work"} />
-                <NavTabV icon={<NumberCircleThree size={26} />} iconPosition="start" label={labelVisibility && "Playground"} />
-                <NavTabV icon={<NumberCircleFour size={26} />} iconPosition="start" label={labelVisibility && "Contact"} />
+                {items.map((item, index) => (
+                  <NavTabV
+                    key={index}
+                    icon={item.icon}
+                    iconPosition="start"
+                    label={labelVisibility && item.label}
+                    onClick={() => window.open(item.pathName, (item.openInNewTab ? "_blank" : "_self"))}
+                  />
+                ))}
               </NavTabsV>
-              {/* IIFE (Immediately Invoked Function Expressions) */}
-              {(() => {
-                switch (value) {
-                  case 0:
-                    // Experience.
-                    break;
-                  case 1:
-                    // Work.
-                    break;
-                  case 2:
-                    // Playground.
-                    break;
-                  case 3:
-                    // Contact.
-                    break;
-                }
-              })()}
             </Stack>
           </Box>
         </Stack>
