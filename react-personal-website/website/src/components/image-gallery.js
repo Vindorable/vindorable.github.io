30a8cda3f0b2fd9b7e0d5286e0f2cc8abd990f55
Vindorable <j.raj93.designer@gmail.com>
2023-12-29 15:50:31 +0800

    Image gallery component now uses the lightbox component.

--- react-personal-website/website/src/components/image-gallery.js
+++ react-personal-website/website/src/components/image-gallery.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Backdrop, Box, Button, Fade, IconButton, ImageList, ImageListItem, Modal, Stack } from "@mui/material";
+import React, { useRef } from "react";
+import { ImageList, ImageListItem } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
-import { CaretCircleLeft, CaretCircleRight, XCircle } from "phosphor-react";
+
+import Lightbox from "./image/lightbox";
 
 
 // ---------------------------------------------------------
@@ -42,38 +43,7 @@ const ImageGallery = ({ imageSet, width, height, columns }) => {
   const _width = width;
   const _height = height;
 
-  // Modal.
-  const [imageItem, setImageItem] = React.useState("false");
-  const [image, setImage] = React.useState("false");
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = (value) => {
-    setImageItem(value);
-    setImage(value.img);
-    setOpen(true);
-  }
-  const handleClose = () => setOpen(false);
-
-  const modalNext = () => {
-    let currentIndex = imageSet.indexOf(imageItem);
-    if (currentIndex >= imageSet.length - 1) {
-      setOpen(false);
-    } else {
-      let nextImageItem = imageSet[currentIndex + 1];
-      setImageItem(nextImageItem);
-      setImage(nextImageItem.img);
-    }
-  };
-
-  const modalBack = () => {
-    let currentIndex = imageSet.indexOf(imageItem);
-    if (currentIndex <= 0) {
-      setOpen(false);
-    } else {
-      let nextImageItem = imageSet[currentIndex - 1];
-      setImageItem(nextImageItem);
-      setImage(nextImageItem.img);
-    }
-  };
+  const lightboxRef = useRef(null);
 
   return (
     <>
@@ -104,89 +74,13 @@ const ImageGallery = ({ imageSet, width, height, columns }) => {
                 borderRadius: "8px",
               }}
               loading="lazy"
-              onClick={(e) => handleOpen(item)}
+              onClick={(e) => lightboxRef.current.openLightbox(item)}
             />
           </ImageListItem>
         ))}
       </ImageListGallery>
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box
-            sx={{
-              width: "100vw",
-              height: "100vh",
-              backgroundColor: "#000",
-            }}
-          >
-            <Box
-              component="img"
-              sx={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                width: image.width,
-                height: "auto",
-                maxWidth: "100%",
-                maxHeight: "90%",
-                bgcolor: "background.paper",
-                border: "1px solid #fff",
-                boxShadow: 24,
-                outline: "none",
-                objectFit: "contain"
-              }}
-              alt=""
-              src={image}
-              style={{ objectFit: "contain", borderRadius: "8px" }}
-            />
-
-            <Stack sx={{ width: "100vw", height: "100vh" }}>
-              <Stack
-                sx={{ width: "100%", height: "100%" }}
-                p={1}
-                justifyContent={"center"}
-              >
-                <Stack
-                  direction={"row"}
-                  alignItems={"center"}
-                  justifyContent={"space-between"}
-                >
-                  <IconButton onClick={() => modalBack()}>
-                    <CaretCircleLeft size={32} />
-                  </IconButton>
-                  <IconButton onClick={() => modalNext()}>
-                    <CaretCircleRight size={32} />
-                  </IconButton>
-                </Stack>
-              </Stack>
-
-              <Stack
-                direction={"row"}
-                sx={{ width: "100%" }}
-                position={"absolute"}
-                p={1}
-                alignItems={"center"}
-                justifyContent={"end"}
-              >
-                <IconButton onClick={() => setOpen(false)}>
-                  <XCircle size={32} />
-                </IconButton>
-              </Stack>
-            </Stack>
-          </Box>
-        </Fade>
-      </Modal>
+      <Lightbox ref={lightboxRef} imageSet={imageSet} />
     </>
   );
 }
