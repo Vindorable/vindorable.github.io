82949f9f4e51f242c7cb6966222e4fe7a406db13
Vindorable <j.raj93.designer@gmail.com>
2024-01-01 18:34:11 +0800

    Updated youtube component to auto center the player.

--- react-personal-website/website/src/components/youtube-embed.js
+++ react-personal-website/website/src/components/youtube-embed.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 
 // https://stackoverflow.com/a/75863103
@@ -7,26 +7,30 @@ import { Box } from "@mui/material";
 
 const YoutubeEmbed = ({ embedId }) => {
   return (
-    <Box
-      sx={{
-        position: "relative",
-        width: "100%",
-        paddingBottom: "56.25%", // 9/16 * 100% for 16:9 aspect ratio
-      }}
-    >
-      <iframe
-        src={`https://www.youtube.com/embed/${embedId}`}
-        title="YouTube video player"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          border: 0,
-        }}
-      />
-    </Box>
+    <Stack width={"100%"} alignItems={"center"} justifyContent={"center"}>
+      <Stack width={"100%"} maxWidth={"600px"}>
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            paddingBottom: "56.25%", // 9/16 * 100% for 16:9 aspect ratio
+          }}
+        >
+          <iframe
+            src={`https://www.youtube.com/embed/${embedId}`}
+            title="YouTube video player"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              border: 0,
+            }}
+          />
+        </Box>
+      </Stack>
+    </Stack>
   );
 }
 
