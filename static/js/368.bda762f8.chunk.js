7d8dd706a0bf10455213e399a45a23e9792ef99e
Vindorable <j.raj93.designer@gmail.com>
2024-01-02 12:01:54 +0800

    Deploy React app to GitHub Pages

--- static/js/368.bda762f8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[368],{5368:(e,s,t)=>{t.r(s),t.d(s,{default:()=>c});t(2791);var a=t(184);const c=()=>(0,a.jsx)(a.Fragment,{children:"Contact"})}}]);
-//# sourceMappingURL=368.bda762f8.chunk.js.map
