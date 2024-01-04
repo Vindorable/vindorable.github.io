8c2943e8be7d519d8b9bbe55162059ef5c9b65a9
Vindorable <j.raj93.designer@gmail.com>
2024-01-04 12:33:38 +0800

    Testing some glitch effects.

--- react-personal-website/website/src/pages/homepage.js
+++ react-personal-website/website/src/pages/homepage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
 
+import { useGlitch } from "react-powerglitch";
+
 import MyAvatar from "../assets/images/homepage/jay-avatar.png"
 import MyName from "../assets/images/homepage/jay-name.png"
 
@@ -8,6 +10,15 @@ import MyName from "../assets/images/homepage/jay-name.png"
 // ---------------------------------------------------------
 
 const Homepage = () => {
+  // https://github.com/7PH/react-powerglitch
+  // https://7ph.github.io/powerglitch/#/playground
+  const glitchText = useGlitch({ shake: { "amplitudeY": 0 } });
+  const glitchAvatar = useGlitch({
+    shake: { "amplitudeY": 0 },
+    timing: { "duration": 4000 },
+    glitchTimeSpan: { "end": 0.6 }
+  });
+
   return (
     <>
       <Stack
@@ -44,12 +55,16 @@ const Homepage = () => {
 
           <Stack height={"2%"}>
             <Typography inline align="center" variant="body2" fontWeight={"300"} fontSize={"1.6vh"} letterSpacing={"0.1rem"}>
-              Welcome to my personal space.
+              <span ref={glitchText.ref}>
+                Welcome to my personal space.
+              </span>
             </Typography>
           </Stack>
 
           <Stack height={"70%"}>
-            <img src={MyAvatar} style={{ objectFit: "cover", height: "100%" }} />
+            <span ref={glitchAvatar.ref} style={{ maxHeight: "calc((100vh - 60px) / 100 * 70)" }}>
+              <img src={MyAvatar} style={{ objectFit: "cover", height: "100%" }} />
+            </span>
           </Stack>
         </Stack>
       </Stack>
