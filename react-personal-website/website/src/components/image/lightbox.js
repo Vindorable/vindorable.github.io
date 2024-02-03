da656d17e70d4a79599b7372be690c372b17a75a
Vindorable <j.raj93.designer@gmail.com>
2024-02-03 17:15:12 +0800

    Lightbox can show some overlay texts.

--- react-personal-website/website/src/components/image/lightbox.js
+++ react-personal-website/website/src/components/image/lightbox.js
@@ -1,5 +1,5 @@
 import React, { forwardRef, useImperativeHandle } from "react";
-import { Backdrop, Box, Fade, IconButton, Modal, Stack } from "@mui/material";
+import { Backdrop, Box, Fade, IconButton, Link, Modal, Stack } from "@mui/material";
 import { CaretCircleLeft, CaretCircleRight, XCircle } from "phosphor-react";
 
 
@@ -14,11 +14,15 @@ const Lightbox = forwardRef(({ imageSet }, ref) => {
 
   const [imageItem, setImageItem] = React.useState("false");
   const [image, setImage] = React.useState("false");
+  const [imageW, setImageW] = React.useState(0);
+  const [imageH, setImageH] = React.useState(0);
+  const [imageHref, setImageHref] = React.useState("");
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = (value) => {
     setImageItem(value);
     setImage(value.img);
+    setImageHref(value.href);
     setOpen(true);
   }
 
@@ -32,6 +36,7 @@ const Lightbox = forwardRef(({ imageSet }, ref) => {
       let nextImageItem = imageSet[currentIndex + 1];
       setImageItem(nextImageItem);
       setImage(nextImageItem.img);
+      setImageHref(nextImageItem.href);
     }
   };
 
@@ -43,8 +48,16 @@ const Lightbox = forwardRef(({ imageSet }, ref) => {
       let nextImageItem = imageSet[currentIndex - 1];
       setImageItem(nextImageItem);
       setImage(nextImageItem.img);
+      setImageHref(nextImageItem.href);
     }
   };
+
+  const onImgLoad = (img) => {
+    //console.log(img.target.offsetHeight)
+    //console.log(img.target.offsetWidth)
+    setImageW(img.target.offsetWidth);
+    setImageH(img.target.offsetHeight);
+  }
 
   return (
     <>
@@ -87,7 +100,45 @@ const Lightbox = forwardRef(({ imageSet }, ref) => {
               alt=""
               src={image}
               style={{ objectFit: "contain", borderRadius: "8px" }}
+              onLoad={onImgLoad}
             />
+
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: imageW,
+                height: imageH,
+                maxWidth: "100%",
+                maxHeight: "90%",
+                //bgcolor: "background.paper",
+                //border: "1px solid #fff",
+              }}
+            >
+              <Stack
+                p={0.75}
+                justifyContent={"end"}
+                sx={{
+                  //bgcolor: "red",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                {imageHref &&
+                  <Box
+                    p={1}
+                    sx={{
+                      bgcolor: "rgba(24, 24, 24, 0.7)",
+                      borderRadius: "8px"
+                    }}
+                  >
+                    <Link target="_blank" href={imageHref} color="inherit" variant="body2">{imageHref}</Link>
+                  </Box>
+                }
+              </Stack>
+            </Box>
 
             <Stack sx={{ width: "100vw", height: "100dvh" }}>
               <Stack
